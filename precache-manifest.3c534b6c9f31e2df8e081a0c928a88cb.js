140e8a21cedb26f321d45b05f7f36f25698e8183
Woldemar <Woldemar8888@gmail.com>
2019-12-24 20:03:08 +0300

    Updates

---
+++ precache-manifest.3c534b6c9f31e2df8e081a0c928a88cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d961a8b204d7218a9ec02aae1cc1801",
+    "url": "/addPost/index.html"
+  },
+  {
+    "revision": "ec28d18f926d59279517",
+    "url": "/addPost/static/css/main.7f07ca56.chunk.css"
+  },
+  {
+    "revision": "e3c36e9260f3e68d3fc1",
+    "url": "/addPost/static/js/2.ab293fbd.chunk.js"
+  },
+  {
+    "revision": "ec28d18f926d59279517",
+    "url": "/addPost/static/js/main.49d1e027.chunk.js"
+  },
+  {
+    "revision": "699f58159401e6178d5f",
+    "url": "/addPost/static/js/runtime-main.c491ed3b.js"
+  }
+]);
